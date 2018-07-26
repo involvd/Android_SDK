4bc8404b9292bd6de208cad7a755073c1db733d0
jj <rob@iamrobj.com>
2018-07-26 07:35:53 +0100

    - Fixed create feature description hint

--- involvd_sdk/src/main/java/com/involvd/sdk/ui/create_feature_request/CreateFeatureRequestFragment.kt
+++ involvd_sdk/src/main/java/com/involvd/sdk/ui/create_feature_request/CreateFeatureRequestFragment.kt
@@ -2,6 +2,9 @@ package com.involvd.sdk.ui.create_bug_report
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.TextInputLayout
+import android.view.View
 import com.involvd.BuildConfig
 import com.involvd.R
 import com.involvd.sdk.data.models.BaseReport
@@ -9,6 +12,11 @@ import com.involvd.sdk.data.models.FeatureRequest
 import com.involvd.sdk.ui.base_report.BaseCreateReportFragment
 
 open class CreateFeatureRequestFragment : BaseCreateReportFragment<FeatureRequest, CreateFeatureRequestView, CreateFeatureRequestPresenter>(), CreateFeatureRequestView {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextInputLayout>(R.id.description_hint).hint = getString(R.string.hint_feature_req_text)
+    }
 
     override fun getSuccessResId(): Int {
         return R.string.create_feature_request_success
